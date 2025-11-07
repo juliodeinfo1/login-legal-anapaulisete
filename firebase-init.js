e381aa3af1a3f281f4a2b097fba718c60a7e88dc
juliodeinfo1 <juliocesardasilvapais3@gmail.com>
2025-11-07 17:19:40 +0000

    a

--- firebase-init.js
+++ firebase-init.js
@@ -1,12 +1,18 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDHI8sWXCb7NIHGeuvVTdY_NkEBOk9s-ns",
-    authDomain: "projetogamificaeduk.firebaseapp.com",
-    projectId: "projetogamificaeduk",
-    storageBucket: "projetogamificaeduk.appspot.com",
-    messagingSenderId: "402375253085",
-    appId: "1:402375253085:web:c882c3e23942e5874801b4",
-    measurementId: "G-2TY4YPL585"
-  };
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCP3KWUodUNeCU5SVeSz4sSxXRrYEzcGUE",
+  authDomain: "carlos-6a7f5.firebaseapp.com",
+  projectId: "carlos-6a7f5",
+  storageBucket: "carlos-6a7f5.firebasestorage.app",
+  messagingSenderId: "461879435011",
+  appId: "1:461879435011:web:0e24ae33b2be0f1b81d336",
+  measurementId: "G-DC5GY3H1HD"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 firebase.initializeApp(firebaseConfig); 
   
